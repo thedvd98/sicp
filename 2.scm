d61c53c0f42d2c5f4d30164bad9c4e73c95060ab
thedvd98 <gianadavide@gmail.com>
2021-04-26 21:19:30 +0200

    same parity

--- 2.scm
+++ 2.scm
@@ -11,3 +11,34 @@
   (if (null? l)
       '()
       (append1 (reverse1 (cdr l)) (list (car l)))))
+
+;ex 2.19
+(define us-coins (list 50 25 10 5 1))
+
+(define (no-more? coin-values)
+  (if (null? coin-values) #t #f))
+
+(define (except-first-denomination coin-values)
+  (cdr coin-values))
+(define (first-denomination coin-values)
+  (car coin-values))
+
+(define (cc amount coin-values)
+  (cond ((= amount 0) 1)
+        ((or (< amount 0) (no-more? coin-values)) 0)
+        (else (+ (cc amount
+                     (except-first-denomination coin-values))
+                 (cc (- amount
+                        (first-denomination coin-values))
+                     coin-values)))))
+
+;ex 2.20
+
+(define (same-parity a . w)
+  (cond ((null? w) '())
+        ((and (even? a) (even? (car w)))
+              (cons (car w) (apply same-parity a (cdr w))))
+        ((and (not (even? a)) (not (even? (car w))))
+         (cons (car w) (apply same-parity a (cdr w))))
+        (else
+          (apply same-parity a (cdr w)))))
