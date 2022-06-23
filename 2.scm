c94369a18df948f81ad71d6905643dfc378ea6cf
thedvd98 <>
2022-06-23 16:43:00 +0200

    2.30

--- 2.scm
+++ 2.scm
@@ -179,3 +179,30 @@
 
 
 
+;; Mapping over trees
+
+(define (scale-tree tree factor)
+  (map (lambda (sub-tree)
+         (if (pair? sub-tree)
+             (scale-tree sub-tree factor)
+             (* sub-tree factor)))
+       tree))
+; 2.30
+(define (square-tree1 tree)
+  (cond
+    ((null? tree) '())
+    ((not (pair? tree)) (* tree tree))
+    (else
+      (cons
+        (square-tree1 (car tree))
+        (square-tree1 (cdr tree))))))
+
+(define (square-tree2 tree)
+  (map (lambda (sub-tree)
+         (if (pair? sub-tree)
+             (square-tree2 sub-tree)
+             (* sub-tree sub-tree)
+             )
+         ) tree))
+
+
