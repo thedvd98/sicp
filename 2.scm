868fd8451f0ace48742d676ade77f7b8861458d7
thedvd98 <>
2022-06-23 22:55:42 +0200

    Sequences as Conventional Interfaces

--- 2.scm
+++ 2.scm
@@ -232,5 +232,42 @@
              (cons (car s) x))
            rest)))))
 
+;; Sequences as Conventional Interfaces
 
+(define (sum-odd-squares tree)
+  (cond ((null? tree) 0)
+        ((not (pair? tree))
+         (if (odd? tree)
+             (square tree)
+             0))
+        (else
+          (+
+            (sum-odd-squares (car tree))
+            (sum-odd-squares (cdr tree))))))
 
+(define (filter predicate sequence)
+  (cond
+    ((null? sequence) '())
+    ((predicate (car sequence))
+     (cons (car sequence) (filter predicate (cdr sequence))))
+    (else
+      (filter predicate (cdr sequence)))))
+
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence) (accumulate op initial (cdr sequence)))))
+
+(define (enumerate-interval low high)
+  (if (> low high)
+      '()
+      (cons low (enumerate-interval (+ low 1) high))))
+
+(define (enumerate-tree tree)
+  (cond
+    ((null? tree) '())
+    ((not (pair? tree)) (list tree))
+    (else
+      (append
+        (enumerate-tree (car tree))
+        (enumerate-tree (cdr tree))))))
